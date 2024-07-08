6a5593e5e320bbf0ab42e7dfac80a7116e2c9040
LAPTOP-7GTIMF9A\adnan <adnan6official@gmail.com>
2024-07-08 14:05:49 +0500

    learned about how to create routes nested routes dynamic routes and how to catch all routes ...

--- src/app/products/[...dynamicProducts]/page.jsx
+++ src/app/products/[...dynamicProducts]/page.jsx
@@ -1,5 +1,9 @@
+"use client";
 import React from "react";
+import { useParams } from "next/navigation";
 
 export default function DynamicProduct() {
+  const params = useParams();
+  console.log(params?.dynamicProducts);
   return <div>Dynamic product page</div>;
 }
